a4f568125d8a32b8396634ea4808ed10a4339882
jishusahootcs <jishuk.sahoo@tcs.com>
2014-04-02 13:05:24 -0700

    fizzbuzz.js created

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Jishu on 02-04-2014.
+ */
+
+var fizbuzz = function(num){
+
+
+    if(num%15==0){
+
+
+        return 'FizzBuzz';
+    }
+    else if(num%3==0){
+
+
+        return 'Fizz';
+    }
+    else if(num%5==0){
+
+
+        return 'Buzz';
+    }
+    else{
+
+
+        return num;
+    }
+}
+module.exports = fizbuzz;
+
